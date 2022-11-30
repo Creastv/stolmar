ab4160a12ba93f0805acf801dbb476b2c5ba7d06
Creastv <piotr@roial.pl>
2022-11-30 13:42:49 +0100

    numbers header

--- template-parts/header/header-contact.php
+++ template-parts/header/header-contact.php
@@ -13,6 +13,6 @@
     <a class="email" href="mailto:<?php echo $header['email']; ?>" title="#">
         <img src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/mail-icon.png"
              alt="E-mail" width="20" height="17">
-        <span><strong><?php echo $header['email']; ?></strong></span>
+        <span><?php echo $header['email']; ?></span>
     </a>
 </div>
